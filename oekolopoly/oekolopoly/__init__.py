a10dacdd6a5a45956cdfbfda9589e2c5c455b273
Ralitsa <71445512+cherrisimo@users.noreply.github.com>
2021-11-25 20:36:15 +0200

    Update __init__.py

--- oekolopoly/oekolopoly/__init__.py
+++ oekolopoly/oekolopoly/__init__.py
@@ -4,10 +4,40 @@ import gym
 env_dict = gym.envs.registration.registry.env_specs.copy()
 for env in env_dict:
     if 'Oekolopoly-v0' in env:
-         #print("Remove {} from registry".format(env))
+         del gym.envs.registration.registry.env_specs[env]
+    if 'OekolopolyRew2-v0' in env:
+         del gym.envs.registration.registry.env_specs[env]
+    if 'OekolopolyRew3-v0' in env:
+         del gym.envs.registration.registry.env_specs[env]
+    if 'OekolopolyRew4-v0' in env:
+         del gym.envs.registration.registry.env_specs[env]
+    if 'OekolopolyRandom-v0' in env:
+         del gym.envs.registration.registry.env_specs[env]
+    if 'OekolopolyRandomRew2-v0' in env:
          del gym.envs.registration.registry.env_specs[env]
 
 register(
     id='Oekolopoly-v0',
     entry_point='oekolopoly.envs:OekoEnv',
 )
+register(
+    id='OekolopolyRew2-v0',
+    entry_point='oekolopoly.envs:OekoEnvRew2',
+)
+register(
+    id='OekolopolyRew3-v0',
+    entry_point='oekolopoly.envs:OekoEnvRew3',
+)
+register(
+    id='OekolopolyRew4-v0',
+    entry_point='oekolopoly.envs:OekoEnvRew4',
+)
+register(
+    id='OekolopolyRandom-v0',
+    entry_point='oekolopoly.envs:OekoEnvRandom',
+)
+register(
+    id='OekolopolyRandomRew2-v0',
+    entry_point='oekolopoly.envs:OekoEnvRandomRew2',
+)
+
