8ae8225d75170a820a66f4751f88e0d813c8a1a0
Ralitsa <71445512+cherrisimo@users.noreply.github.com>
2021-11-25 20:37:07 +0200

    Update __init__.py

--- oekolopoly/oekolopoly/envs/__init__.py
+++ oekolopoly/oekolopoly/envs/__init__.py
@@ -1,1 +1,4 @@
-from oekolopoly.envs.oeko_env     import OekoEnv
+from oekolopoly.envs.oeko_env             import OekoEnv
+from oekolopoly.envs.oeko_rew2_env        import OekoEnvRew2
+from oekolopoly.envs.oeko_rew3_env        import OekoEnvRew3
+from oekolopoly.envs.oeko_rew4_env        import OekoEnvRew4
