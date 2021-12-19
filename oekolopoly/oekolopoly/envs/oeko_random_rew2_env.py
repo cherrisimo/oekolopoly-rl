df468264f30ebb2f2311ed89734bcdb5be403031
Ralitsa <71445512+cherrisimo@users.noreply.github.com>
2021-12-19 23:37:24 +0100

    Add oeko_random_rew2_env

---
+++ oekolopoly/oekolopoly/envs/oeko_random_rew2_env.py
@@ -0,0 +1,8 @@
+from oekolopoly.envs.oeko_random_env import OekoEnvRandom
+from oekolopoly.envs.oeko_rew2_env   import OekoEnvRew2
+import numpy as np
+import random
+
+
+class OekoEnvRandomRew2 (OekoEnvRandom, OekoEnvRew2):
+    pass
