4528e490d5d59cda386e7d8d67b0f4cd6fd2f126
Ralitsa <71445512+cherrisimo@users.noreply.github.com>
2021-12-20 00:31:40 +0200

    Update oeko_rew2_env.py

--- oekolopoly/oekolopoly/envs/oeko_rew2_env.py
+++ oekolopoly/oekolopoly/envs/oeko_rew2_env.py
@@ -3,7 +3,7 @@ from oekolopoly.envs.oeko_env import OekoEnv
 
 class OekoEnvRew2(OekoEnv):
     def get_oeko_reward (self, done):
-        if done:
+        if done and self.V[self.ROUND] in range(10, 31):
             return self.balance
         else:
             return 1
