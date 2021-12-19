95ec08be5a3810f4fe50214afa9ad0311089924a
Ralitsa <71445512+cherrisimo@users.noreply.github.com>
2021-12-20 00:35:31 +0200

    Update __init__.py

--- oekolopoly/oekolopoly/envs/__init__.py
+++ oekolopoly/oekolopoly/envs/__init__.py
@@ -2,3 +2,5 @@ from oekolopoly.envs.oeko_env             import OekoEnv
 from oekolopoly.envs.oeko_rew2_env        import OekoEnvRew2
 from oekolopoly.envs.oeko_rew3_env        import OekoEnvRew3
 from oekolopoly.envs.oeko_rew4_env        import OekoEnvRew4
+from oekolopoly.envs.oeko_random_env      import OekoEnvRandom
+from oekolopoly.envs.oeko_random_rew2_env import OekoEnvRandomRew2
