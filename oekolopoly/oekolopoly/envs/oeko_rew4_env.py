0e7517955297f76e40826b1d37ba49be630c31cc
Ralitsa <71445512+cherrisimo@users.noreply.github.com>
2021-12-20 00:32:27 +0200

    Update oeko_rew4_env.py

--- oekolopoly/oekolopoly/envs/oeko_rew4_env.py
+++ oekolopoly/oekolopoly/envs/oeko_rew4_env.py
@@ -3,7 +3,7 @@ from oekolopoly.envs.oeko_env import OekoEnv
 
 class OekoEnvRew4(OekoEnv):
     def get_oeko_reward (self, done):
-        if done:
+        if done and self.V[self.ROUND] in range(10, 31):
             return self.balance
         else:
             production_reward   = 14 - abs (15 - self.V[self.PRODUKTION])
